faa7d843a7e4a87534d41160bfca3848a1fb54ef
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-07 01:41:30 +0300

    implementing LL: Set

--- src/com/luv2code/linkedlist/Main.java
+++ src/com/luv2code/linkedlist/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LinkedList myLinkedList = new LinkedList(0);
-		myLinkedList.append(1);
-		myLinkedList.append(2);
+		LinkedList myLinkedList = new LinkedList(11);
 		myLinkedList.append(3);
-		System.out.println(myLinkedList.get(2).value+"\n");
+		myLinkedList.append(23);
+		myLinkedList.append(7);
+		myLinkedList.set(1, 4);
 		myLinkedList.printList();
 	}
 
