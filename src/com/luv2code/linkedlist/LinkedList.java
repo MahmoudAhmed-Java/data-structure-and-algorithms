62abc0bdbbbffe87071b6dd8338e70675fc5151c
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-07 01:23:04 +0300

    implementing LL: Remove First

--- src/com/luv2code/linkedlist/LinkedList.java
+++ src/com/luv2code/linkedlist/LinkedList.java
@@ -83,4 +83,14 @@ public class LinkedList {
 			head = newNode;
 		}
 	}
+	
+	public Node removeFirst() {
+		if( length == 0 ) return null;
+		Node temp = head;
+		head = head.next;
+		temp.next = null;
+		length--;
+		if( length == 0 ) tail = null;
+		return temp;
+	}
 }
--- src/com/luv2code/linkedlist/Main.java
+++ src/com/luv2code/linkedlist/Main.java
@@ -5,13 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedList myLinkedList = new LinkedList(2);
-		
-		myLinkedList.append(3);
-		myLinkedList.prepend(1);
-		
-		myLinkedList.printList();
-		
-		
+		myLinkedList.append(1);
+		System.out.println(myLinkedList.removeFirst().value);
+		System.out.println(myLinkedList.removeFirst().value);
+		System.out.println(myLinkedList.removeFirst());
 	}
 
 }
