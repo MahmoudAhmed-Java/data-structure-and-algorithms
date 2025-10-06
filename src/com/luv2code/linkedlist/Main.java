85c525d2140ede22ec18b2f586c97e5e20caa443
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-06 23:49:46 +0300

    implementing LL: Print List

--- src/com/luv2code/linkedlist/Main.java
+++ src/com/luv2code/linkedlist/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedList myLinkedList = new LinkedList(4);
+		
+		myLinkedList.getHead();
+		myLinkedList.getTail();
+		myLinkedList.getLength();
+		
+		myLinkedList.printList();
 	}
 
 }
