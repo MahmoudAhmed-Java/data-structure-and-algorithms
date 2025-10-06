9c82abdf3bbd6c56ef36a697849bc58871994576
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-06 21:23:27 +0300

    explain bigO: O(1)

--- src/com/luv2code/demo/Main.java
+++ src/com/luv2code/demo/Main.java
@@ -2,21 +2,14 @@ package com.luv2code.demo;
 
 public class Main {
 
-	public static void printItems(int n) {
-		for( int i = 0 ; i < n ; i++) {
-			for( int j = 0 ; j < n ; j++) {
-				System.out.println(i+" "+j);
-			}
-		}
-		for( int k = 0 ; k < n ; k++) {
-			System.out.println(k);
-		}
-		
+	
+	public static int addItems( int m) {
+		return m + m ;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printItems(10);
+		System.out.println(addItems(10));
 	}
 
 }
