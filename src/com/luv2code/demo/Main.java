ac99784b6f5df5a8632a8304aea6febbd6788664
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-06 19:54:32 +0300

    explain big O(n)

--- src/com/luv2code/demo/Main.java
+++ src/com/luv2code/demo/Main.java
@@ -2,9 +2,15 @@ package com.luv2code.demo;
 
 public class Main {
 
+	public static void printItems(int n) {
+		for( int i = 0 ; i < n ; i++) {
+			System.out.println(i);
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello My Course Data Structure And Algorithms : Mahmoud Ahmed");
+		printItems(10);
 	}
 
 }
