ad3b50b7f235c70a02ba83dfc8beb2b601128869
MahmoudAhmed-Java <mahmoudahmed345as@gmail.com>
2025-10-06 20:45:11 +0300

    explain bigO:drop constants

--- src/com/luv2code/demo/Main.java
+++ src/com/luv2code/demo/Main.java
@@ -6,6 +6,9 @@ public class Main {
 		for( int i = 0 ; i < n ; i++) {
 			System.out.println(i);
 		}
+		for( int j = 0 ; j < n ; j++) {
+			System.out.println(j);
+		}
 	}
 	
 	public static void main(String[] args) {
